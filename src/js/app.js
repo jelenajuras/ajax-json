e15b5ac75b63e6fd7c90a40146a73fcc2e0266c0
programer10 <programer10@ucione.local>
2018-11-28 21:30:23 +0100

    ajax request

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,55 @@
-var handleNavbar = function (element) {
-	$(window).scroll(function(){              //event
-		if($(window).scrollTop() > 100){
-			$(element).css({
-				'padding-top': 0,
-				'padding-bottom': 0,
-				'background-color': '#fff'
-				});
-				$(element + " a").css('color','#111')
-		} else {
-			//brži naèin  $(element).removeAttr('style');
-			$(element).css({
-				'padding-top': "3rem",
-				'padding-bottom': "3rem",
-				'background-color': 'transparent'
-				});
-				$(element + " a").css('color','#3b4861')
-		}
-	});		   
-}
+var handleContent = function () {
+	var preloader = $('.preloader');
+	var ayaxLoader = '<img class="ajax-loader" src="/img/ajax-loader.gif" />';
+	
+	$('.btn').click(function(e){
+		e.preventDefault();
+		
+		$.ajax({
+			url: "data/user.json",
+			type: "GET",
+			data: {"id": 1},
+			beforeSend: function(){
+				preloader.empty();
+				preloader.append(ayaxLoader);
+			},
+			success: function(result, status, xhr){
+				//console.log(result);
+			},
+			error:function(xhr, status, error){
+				if(error){
+					preloader.empty();
+					preloader.text("An error occured while poccessing your requesr! Please try again later!");
+					setTimeout(function() {
+						preloader.empty();
+					},5000);
+				}
+			},
+			complete: function(result){
+				preloader.empty();
+				console.log(result.responseJSON);
+				
+				var text;
+				for (x in result.responseJSON) {
+					text = "<tr><td>" + resutl.[x]["id"] + "</td>";
+					text += "<td>" + resutl.[x]["name"] + "</td>";
+					text += "<td>" + resutl.[x]["username"] + "</td>";
+					text += "<td>" + resutl.[x]["email"] + "</td></tr>";
+					$(".tbody").append(text);
+					console.log(text);
+				};
+				
+				
+			}
+		});
+	});
+};
 
 var App = function () {
     return {
         init: function (element){
-			handleNavbar(element);
+			handleContent();
         },
     }
 }(); 
 
-
-//append - dodaje na kraj
-//prepend - dodaje na poèetak
